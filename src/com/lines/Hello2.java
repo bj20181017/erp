b3daff8ff8b317f9a69830c29882434a82dc389c
bj <bj910521@163.com>
2018-10-23 17:24:37 +0800

    新建文件

--- src/com/lines/Hello2.java
+++ src/com/lines/Hello2.java
@@ -1,9 +1,8 @@
 package com.lines;
 
-public class Hello {
+public class Hello2 {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
 		System.out.println("hello world");
 	}
 
